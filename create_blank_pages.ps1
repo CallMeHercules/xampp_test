1613b17d659bc1de2697a7a276b56eb675a2d32e
Michael s <msullivan255@gmail.com>
2023-08-17 20:47:22 -0500

    initial push

---
+++ create_blank_pages.ps1
@@ -0,0 +1,11 @@
+type nul > index.php
+mkdir css
+mkdir js
+mkdir images
+cd css
+type nul > style.css
+cd ../js
+type nul > script.js
+cd ../images
+type nul > image.jpg
+echo "Project structure and files created successfully."
